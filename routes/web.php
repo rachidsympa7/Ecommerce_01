b18cd94238304ff5977561de1934a89d1acbaf46
DIrachid <rachidox.ochella@gmail.com>
2026-08-13 22:56:40 +0100

    feat: add product route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('products', [ProductController::class, 'index'])->name('products.index');
